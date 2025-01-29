7d2628eb49f4408406440002805810a51f551ef9
RenanMartins7 <renanlol.sl@gmail.com>
2025-01-29 16:32:21 -0300

    gauges added

--- api.py
+++ api.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from merge import *
 from selection import *
 from iperf import *
 from otlp_provider import *
 from metrics import *
+
+from opentelemetry import metrics
+
+
 import random
+
+from typing import Iterable
 
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
@@ -14,53 +20,50 @@ os.environ["OTEL_SERVICE_NAME"] = "api"
 #-----------------------------------------Traces, metrics and app service creation-------------------------
 tracer = traces_provider(resource)
 meter = metrics_provider(resource)
-metrics = prometheus_metrics(meter)
+p_metrics = prometheus_metrics(meter)
 app = FastAPI()
-
 
 
 #-----------------------------------------Services Implementation------------------------------------------
 #-----------------------------------------------Merge Sort-------------------------------------------------
 @app.get("/merge")
 def merge_service(size: int = Query(10000, ge=1)):
-
-    metrics.total_requests_add(1, "/merge")
-    metrics.active_requests_add(1)
+    p_metrics.total_requests_add(1, "/merge")
 
     with tracer.start_as_current_span("mergesort", kind=trace.SpanKind.SERVER) as child:
-
         random_list = [random.randint(1, size) for _ in range(size)]
         merge_sort(random_list)
         child.set_attribute("teste", 12)
-    
-    metrics.active_requests_add(-1)
+
     return {"message": "Merge sort completed"}
 
 #-----------------------------------------------Selection Sort------------------------------------------------
 @app.get("/selection")
 def selection_service(size: int= Query(10000, ge=1)):
-
-    metrics.total_requests_add(1, "/selection")
-    metrics.active_requests_add(1)
+    p_metrics.total_requests_add(1, "/selection")
 
     with tracer.start_as_current_span("selectionsort", kind=trace.SpanKind.SERVER) as child:
-
         random_list = [random.randint(1, size) for _ in range(size)]
         selection_sort(random_list)
 
-    metrics.active_requests_add(-1)
     return {"message": "Selection sort completed"}
 
 #---------------------------------------------------Iperf-----------------------------------------------------
 @app.get("/iperftest")
 def iperf_service(server: str = Query("0.0.0.0"), port: int = Query(5201, ge=1), duration: int = Query(10, ge=1)):
+    p_metrics.total_requests_add(1, "/iperf")
     
-    metrics.total_requests_add(1, "/iperf")
-    metrics.active_requests_add(1)
-
     with tracer.start_as_current_span("iperf", kind=trace.SpanKind.SERVER) as child:
-
         run_iperf(server, port, duration)
     
-    metrics.active_requests_add(-1)
     return {"message": "Iperf test completed"}
+
+#------------------------------------------------MiddleWare for active requests--------------------------------
+@app.middleware("http")
+async def count_active_requests(request:Request, call_next):
+    p_metrics.active_requests_add(1)
+    response = await call_next(request)
+    p_metrics.active_requests_add(-1)
+
+    return response
+    
--- metrics.py
+++ metrics.py
@@ -1,3 +1,33 @@
+import psutil
+from opentelemetry.metrics import Observation, CallbackOptions
+from typing import Iterable
+
+def mem_percentage(options: CallbackOptions) -> Iterable[Observation]:
+
+    memory_info = psutil.virtual_memory()
+    mem_perc = memory_info.percent
+
+    yield Observation(mem_perc, {})
+
+def cpu_percentage(option: CallbackOptions) -> Iterable[Observation]:
+
+    cpu_usage = psutil.cpu_percent(interval=1)
+
+    yield Observation(cpu_usage, {})
+
+def mem_total(option: CallbackOptions) -> Iterable[Observation]:
+
+    memory_info = psutil.virtual_memory()
+    mem_total = memory_info.total
+
+    yield Observation(mem_total, {})
+
+def mem_current(option: CallbackOptions)-> Iterable[Observation]:
+    memory_info = psutil.virtual_memory()
+    mem_c = memory_info.used
+
+    yield Observation(mem_c, {})
+
 class prometheus_metrics:
     def __init__(self, meter):
 
@@ -9,6 +39,24 @@ class prometheus_metrics:
             "api_active_requests", unit = "1", description="Number of current active requests"
         )
 
+        self.mem_perc = meter.create_observable_gauge(
+            "mem_percentage", [mem_percentage]
+        )
+
+        self.cpu_perc = meter.create_observable_gauge(
+            "cpu_percentage", [cpu_percentage]
+        )
+
+        self.mem_total = meter.create_observable_gauge(
+            "mem_total", [mem_total]
+        )
+
+        self.mem_used = meter.create_observable_gauge(
+            "mem_used", [mem_current]
+        )
+
+        
+
     def total_requests_add(self,value,endpoint):
         self.total_requests.add(
             value, attributes={"method":"GET", "endpoint": endpoint}
@@ -16,3 +64,4 @@ class prometheus_metrics:
     
     def active_requests_add(self, value):
         self.active_requests.add(value)
+    
